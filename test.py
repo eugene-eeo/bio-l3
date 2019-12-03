6abe8f1bbf3fbbe022d327da029663a0d1aa54c3
Eeo Jun <141bytes@gmail.com>
2019-12-03 00:37:51 +0000

    update

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+import random
 from alignment2 import dynprog, dynproglin, heuralign
 
 
@@ -31,3 +32,29 @@ for s, t in params2:
         a = f(alphabet, scores, s, t)
         print("Score:  ", a[0])
         print("Indices:", a[1], a[2])
+
+
+print()
+print("RANDOMISED TESTING")
+print()
+
+
+for x in range(5000):
+    m = random.randint(0, 50)
+    n = random.randint(0, 50)
+    s = "".join(random.choice(alphabet) for _ in range(m))
+    t = "".join(random.choice(alphabet) for _ in range(n))
+    print(x, m, n)
+
+    # Just check if it runs
+    score3, _, _ = heuralign(alphabet, scores, s, t)
+
+    score1, _, _ = dynprog(alphabet, scores, s, t)
+    score2, _, _ = dynproglin(alphabet, scores, s, t)
+
+    if score1 != score2 or score3 > score1:
+        print(s)
+        print(t)
+        print(score1)
+        print(score2)
+        print(score3)
